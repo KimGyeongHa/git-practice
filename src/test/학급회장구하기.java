8414499af1974de2a37d98527d1bc70edaf5658b
kimGyeongHa <rudgkrk11@naver.com>
2023-01-09 17:29:53 +0900

    Add Conding Test

---
+++ src/test/학급회장구하기.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 학급회장구하기 {
+	
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		
+	}
+
+}
